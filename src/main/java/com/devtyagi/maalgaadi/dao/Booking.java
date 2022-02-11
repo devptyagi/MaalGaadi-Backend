17aef55e3beebdb0de27d70f76efb82d6ac1c848
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2022-02-12 01:19:12 +0530

    Booking DAO created

---
+++ src/main/java/com/devtyagi/maalgaadi/dao/Booking.java
@@ -0,0 +1,42 @@
+package com.devtyagi.maalgaadi.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "bookings")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Booking {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid2")
+    private String bookingId;
+
+    @OneToOne
+    @JoinColumn(name = "dealer_id", referencedColumnName = "dealerId")
+    private Dealer dealer;
+
+    @OneToOne
+    @JoinColumn(name = "driver_id", referencedColumnName = "driverId")
+    private Driver driver;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date bookedAt;
+
+    private String fromCity;
+
+    private String toCity;
+
+}
