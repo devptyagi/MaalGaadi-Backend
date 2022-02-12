50e8790da4216d3648c0f82de7c91b94de46598f
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2022-02-13 01:10:49 +0530

    Fixed OTP login error

--- src/main/java/com/devtyagi/maalgaadi/dto/request/LoginViaOtpRequestDTO.java
+++ src/main/java/com/devtyagi/maalgaadi/dto/request/LoginViaOtpRequestDTO.java
@@ -1,16 +1,21 @@
 package com.devtyagi.maalgaadi.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
 public class LoginViaOtpRequestDTO {
 
     @NotBlank(message = "Username must not be blank!")
     private String username;
 
-    @NotBlank(message = "OTP not be blank!")
+    @Min(value = 100000, message = "OTP Must be 6 digits")
+    @Max(value = 900000, message = "OTP Must be 6 digits")
     private Integer otp;
 
 }
